2c1de018fac477a8d344c09123f5ff6f3d4da710
Manuel Barone <baronemanuel05@gmail.com>
2024-10-15 18:49:46 +0200

    gf

--- src/main/java/sarapavo/Application.java
+++ src/main/java/sarapavo/Application.java
@@ -26,39 +26,27 @@ public class Application {
 //        dao.save(new User("Giulia", "Neri", LocalDate.of(1993, 8, 5), false));
 //        dao.save(new User("Andrea", "Gialli", LocalDate.of(2000, 3, 30), true));
 
-        User user1 = new User("Giovanni", "Rosa", LocalDate.of(1990, 4, 12), false);
-        dao.save(user1);
+//        User user1 = new User("Giovanni", "Rosa", LocalDate.of(1990, 4, 12), false);
+//        dao.save(user1);
+//        User user2 = new User("Anna", "Blu", LocalDate.of(1980, 7, 9), false);
+//        dao.save(user2);
+//        User user3 = new User("Giorgia", "Verdi", LocalDate.of(1975, 11, 22), false);
+//        dao.save(user3);
+//        User user4 = new User("Sandro", "Giallo", LocalDate.of(1999, 8, 15), true);
+//        dao.save(user4);
+//        User user5 = new User("Franco", "Nero", LocalDate.of(1985, 1, 30), false);
+//        dao.save(user5);
+//        dao.save(new Tessera(user1));
+//        dao.save(new Tessera(user2));
+//        dao.save(new Tessera(user3));
+//        dao.save(new Tessera(user4));
+//        dao.save(new Tessera(user5));
 
-        User user2 = new User("Anna", "Blu", LocalDate.of(1980, 7, 9), false);
-        dao.save(user2);
-
-        User user3 = new User("Giorgia", "Verdi", LocalDate.of(1975, 11, 22), false);
-        dao.save(user3);
-
-        User user4 = new User("Sandro", "Giallo", LocalDate.of(1999, 8, 15), true);
-        dao.save(user4);
-
-        User user5 = new User("Franco", "Nero", LocalDate.of(1985, 1, 30), false);
-        dao.save(user5);
-
-        dao.save(new Tessera(user1));
-        dao.save(new Tessera(user2));
-        dao.save(new Tessera(user3));
-        dao.save(new Tessera(user4));
-        dao.save(new Tessera(user5));
-
-
-//                dao.save(new Tessera(new User("Giovanni", "Rosa", LocalDate.of(1990, 4, 12), false)));
-//        dao.save(new Tessera(new User("Anna", "Blu", LocalDate.of(1980, 7, 9), false)));
-//        dao.save(new Tessera(new User("Giorgia", "Verdi", LocalDate.of(1975, 11, 22), false)));
-//        dao.save(new Tessera(new User("Sandro", "Giallo", LocalDate.of(1999, 8, 15), true)));
-//        dao.save(new Tessera(new User("Franco", "Nero", LocalDate.of(1985, 1, 30), false)));
-//
-//                dao.save(new DistributoreAutomatico("Punto K",false));
-//        dao.save(new Rivenditore("Punto L"));
-//        dao.save(new DistributoreAutomatico("Punto M",true));
-//        dao.save(new Rivenditore("Punto N"));
-//        dao.save(new DistributoreAutomatico("Punto O",false));
+        dao.save(new DistributoreAutomatico("Punto K",false));
+        dao.save(new Rivenditore("Punto L"));
+        dao.save(new DistributoreAutomatico("Punto M",true));
+        dao.save(new Rivenditore("Punto N"));
+        dao.save(new DistributoreAutomatico("Punto O",false));
 //
 //                dao.save(new DistributoreAutomatico("Distributore 1", false));
 //        dao.save(new DistributoreAutomatico("Distributore 2", true));
