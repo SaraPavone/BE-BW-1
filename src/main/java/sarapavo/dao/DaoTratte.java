6f0f942e36698cd110c687bddd1a8aaebe7f6255
Manuel Barone <baronemanuel05@gmail.com>
2024-10-17 15:23:45 +0200

    create dao tratte

---
+++ src/main/java/sarapavo/dao/DaoTratte.java
@@ -0,0 +1,26 @@
+package sarapavo.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+public class DaoTratte {
+    private final EntityManager em;
+
+    public DaoTratte(EntityManager em) {
+        this.em = em;
+    }
+
+    public void getTempoMedioTratta(String zonaPartenza, String capolinea){
+        TypedQuery<Double> query = em.createNamedQuery("Tratta.averageTravelTimeForSpecificRoute", Double.class);
+        query.setParameter("zonaPartenza", zonaPartenza);
+        query.setParameter("capolinea",capolinea);
+
+        Double avgTempoPrevisto = query.getSingleResult();
+
+        if(avgTempoPrevisto != null){
+            System.out.println("Tempo medio previsto per la tratta " + zonaPartenza + " - " + capolinea + ": " + avgTempoPrevisto + " minuti");
+        }else{
+            System.out.println("Non ci sono dati per questa tratta");
+        }
+    }
+}
